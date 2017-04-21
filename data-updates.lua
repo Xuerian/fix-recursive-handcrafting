07d4e2798de7ade59ee79d23c473e8da66628e3d
Joe Howanski <joe.howanski@dappereagle.com>
2017-04-21 19:49:49 -0400

    Fixed for current version of Bob's

--- data-updates.lua
+++ data-updates.lua
@@ -1,14 +1,14 @@
--- Force wooden board alternatives to be created in assemblers
+-- Force Wooden Boards made from Synthetic to be made in Electronics machine, Force Cellulose wood to be created in assemblers
 
 local function force_assembler(recipe)
-	if data.raw.recipe[recipe] then
-		data.raw.recipe[recipe].category = "advanced-crafting"
-	end
+    if data.raw.recipe[recipe] then
+        data.raw.recipe[recipe].category = "electronics-machine"
+    end
 end
 
 -- Bob's Plates
 -- Synthetic wood from oil
-force_assembler("synthetic-wood")
+force_assembler("wooden-board-synthetic")
 
 -- Angel's Bioprocessing
 force_assembler("wood-from-cellulose-resin")
